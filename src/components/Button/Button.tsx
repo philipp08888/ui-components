e0b73a92ac13f09f556f49bc8aff44aa9daafc7a
Philipp <admin@fledi.dev>
2024-06-17 00:57:14 +0200

    feat(button): improve code

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,30 @@
-import _ from "classnames";
+import classnames from "classnames";
 import React from "react";
 import styles from "./Button.module.scss";
 
 export interface ExtendedButtonProps {
+  /**
+   * variant of button
+   * @default "primary"
+   */
   variant?: "primary" | "success" | "danger" | "warning";
 }
 
 export type ButtonProps = ExtendedButtonProps &
   React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({
-  variant = "primary",
-  ...props
-}: ButtonProps): JSX.Element => {
-  return (
-    <div className={styles.container}>
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant = "primary", children, className, ...props }, ref) => {
+    return (
       <button
-        className={_(styles.button, styles[variant], props.className)}
+        ref={ref}
+        className={classnames(styles.button, styles[variant], className)}
         {...props}
       >
-        {props.children}
+        {children}
       </button>
-    </div>
-  );
-};
+    );
+  }
+);
+
+Button.displayName = "Button";
